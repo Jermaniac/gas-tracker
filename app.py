fee878cb2980477356e1d766b944f04eca7e5606
Jermaniac <germansk98@gmail.com>
2024-06-25 01:27:41 +0200

    feat - gunicorn fix 6

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
 from app.data_handler import get_filtered_gas_data_by_province
 from flask import Flask, jsonify
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route('/province/<string:id_provincia>')
-    def get_info_province(id_provincia):
-        try:
-            data = get_filtered_gas_data_by_province(id_provincia)
-            return jsonify({"info": data})
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
+@app.route('/province/<string:id_provincia>')
+def get_info_province(id_provincia):
+    try:
+        data = get_filtered_gas_data_by_province(id_provincia)
+        return jsonify({"info": data})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
     
-    @app.errorhandler(404)
-    def page_not_found(e):
-        return jsonify({"error": "Resource not found"}), 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({"error": "Resource not found"}), 404
 
-    @app.errorhandler(500)
-    def internal_server_error(e):
-        return jsonify({"error": "Internal server error"}), 500
-    
-    return app
-
-app = create_app()  # Create the app instance
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9004)
