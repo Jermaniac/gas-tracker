fb59e2b3bfe03fa160074be6dfb9fca1ca53f09e
Jermaniac <germansk98@gmail.com>
2024-06-29 20:45:33 +0200

    fix - deleted unused prop

--- app/ui/dashboard/client-wrapper.tsx
+++ app/ui/dashboard/client-wrapper.tsx
@@ -7,20 +7,12 @@ import { CardsSkeleton } from '@/app/ui/skeletons';
 
 const ClientSideWrapper = () => {
     const [selectedProvince, setSelectedProvince] = useState<number>(28);
-    const [loading, setLoading] = useState<boolean>(false);
 
     return (
         <div>
             <h1 className="mb-4 text-xl md:text-2xl">
                 <ProvincesDropdown onChange={setSelectedProvince} />
             </h1>
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                {loading ? (
-                    <CardsSkeleton />
-                ) : (
-                    <CardWrapper provinceId={selectedProvince} setLoading={setLoading} />
-                )}
-            </div>
         </div>
     );
 };
