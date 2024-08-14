a4491af0dc9ea8ef05e3b3cca7ef10e53f47865b
Jermaniac <germansk98@gmail.com>
2024-08-14 19:33:38 +0200

    fix - deleted unused useMemo

--- app/ui/dashboard/map-gas.tsx
+++ app/ui/dashboard/map-gas.tsx
@@ -1,16 +1,15 @@
 
 import dynamic from 'next/dynamic';
-import { useMemo } from 'react';
 
-export default function MapGas(props: any) {
+export default function MapGas (props: any) {
     const Map = () => dynamic(
         () => import('../../ui/map/index'),
         {
             loading: () => <p>A map is loading</p>,
             ssr: false
         }
-      )
+    )
     return (
         <Map posix={props.posix} list={props.list} />
-        );
+    );
 }
