62bef6872727d93ac0836b507fc7e80a14f777ef
Jermaniac <germansk98@gmail.com>
2024-08-17 22:54:47 +0200

    fix - delete unused imports

--- app/ui/dashboard/gaspricesinfo.tsx
+++ app/ui/dashboard/gaspricesinfo.tsx
@@ -1,8 +1,5 @@
 import { fetchCardData } from '@/app/lib/data';
 import dynamic from 'next/dynamic';
-import CardWrapper from './cards';
-import GasStationList from './gas-station-list';
-import { FUEL_TYPES } from '@/app/lib/constants';
 import ClientComponent from './client-component';
 
 const Map = dynamic(() => import('../map/index'), { ssr: false });
@@ -13,12 +10,6 @@ export default async function GasPricesInfo ({ provinceId }: { provinceId: strin
         <>
             {averages && list.length != 0 && <>
                 <ClientComponent averages={averages} list={list}></ClientComponent>
-                {/* <div className="mt-6">
-                    <GasStationList list={list} />
-                </div>
-                <div>
-                    <Map posix={[40.416775, -3.703790]} list={list} />
-                </div> */}
             </>}
         </>
     );
