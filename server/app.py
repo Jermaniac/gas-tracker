d606a8c5951b6b4bcb3ab21d15259082d2dbdd3d
Jermaniac <germansk98@gmail.com>
2024-08-31 18:07:52 +0200

    fix - log error in exception case, remove stringify

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from utils.data_handler import get_filtered_gas_data_by_province
-from flask import Flask, jsonify
+from flask import Flask
 from flask_caching import Cache
 
 ip = '0.0.0.0'
@@ -11,17 +11,17 @@ cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 def get_info_province(id_provincia):
     try:
         data = get_filtered_gas_data_by_province(id_provincia)
-        return jsonify({"gasStationInfo": data})
+        return ({"gasStationInfo": data})
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return ({"error": str(e)}), 500
     
 @app.errorhandler(404)
 def page_not_found(e):
-    return jsonify({"error": "Resource not found"}), 404
+    return ({"error": "Resource not found"}), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return jsonify({"error": "Internal server error"}), 500
+    return ({"error": "Internal server error"}), 500
 
 if __name__ == "__main__":
     app.run(host=ip, port=9004)
